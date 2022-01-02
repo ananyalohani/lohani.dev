dda94956b86ab60ba40d9b8efd8ec777aee9ac2f
Ananya Lohani <ananya19018@iiitd.ac.in>
2022-01-02 15:19:19 +0530

    styling: Revert to old blog card styling

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -10,15 +10,15 @@ interface Props {
 
 export default function PostCard({ heading, blurb, link, image }: Props) {
   return (
-    <div className='transition-all rounded-md cursor-pointer group sm:max-w-1/2'>
+    <div className='transition-all cursor-pointer group sm:max-w-1/2 bg-gray-50 p-4 border-2 border-gray-200 rounded-lg w-1/2 hover:bg-sky-50 hover:border-sky-200'>
       <Link href={link}>
         <a className='space-y-5 btn-link'>
-          <img src={image} className='rounded-lg' alt={heading} />
+          <img src={image} className='' alt={heading} />
           <div className='space-y-2'>
-            <h2 className='text-lg leading-6 transition-all md:text-xl group-hover:text-sky-400'>
+            <h2 className='text-base leading-6 transition-all md:text-xl group-hover:text-sky-500'>
               {heading}
             </h2>
-            <p className='text-sm text-gray-700 truncate transition-all md:text-base overflow-ellipsis'>
+            <p className='text-sm text-gray-700 truncate transition-all md:text-base overflow-ellipsis group-hover:text-sky-900'>
               {blurb}
             </p>
           </div>
