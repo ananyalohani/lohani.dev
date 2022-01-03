35f4d7411e6dd33ee25fbd6cbd317abbcb44b95f
Ananya Lohani <ananya19018@iiitd.ac.in>
2022-01-04 01:11:06 +0530

    fix: Revert to old ProjectCard styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,11 +17,11 @@ export default function ProjectCard({ project }: Props) {
   const { title, links, description, image, tags } = project;
 
   return (
-    <div className='w-full p-4 transition-all border-2 rounded-lg cursor-pointer bg-slate-50 sm:p-6 border-slate-200 group hover:bg-sky-50 hover:border-sky-200 '>
+    <div className='w-full p-4 transition-all border-2 rounded-lg bg-slate-50 sm:p-6 border-slate-200'>
       <div className='flex flex-col-reverse sm:flex-row sm:space-x-3 btn-link'>
         <div className='flex flex-col space-y-2'>
           <div className='flex flex-row items-center'>
-            <h2 className='text-xl leading-6 transition-all md:text-2xl group-hover:text-sky-500'>
+            <h2 className='text-xl leading-6 transition-all md:text-2xl'>
               {title}
             </h2>
             <div className='flex flex-row items-center ml-3 space-x-2'>
@@ -50,13 +50,13 @@ export default function ProjectCard({ project }: Props) {
               ))}
             </div>
           </div>
-          <p className='text-sm transition-all text-slate-700 md:text-base group-hover:text-sky-900'>
+          <p className='text-sm transition-all text-slate-700 md:text-base'>
             {description}
           </p>
           <div className='flex flex-row flex-wrap gap-2'>
             {tags.map((tag) => (
               <div
-                className='px-2 py-1 text-xs border rounded bg-sky-100 text-sky-500 border-sky-200 md:text-sm group-hover:bg-sky-200 group-hover:border-sky-300'
+                className='px-2 py-1 text-xs border rounded bg-sky-100 text-sky-500 border-sky-200 md:text-sm'
                 key={tag}
               >
                 {tag}
